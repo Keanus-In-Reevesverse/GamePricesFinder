64d5857c7fd6872adba8f38c8e8a9ca3827ee2a6
RicardoKCavati <ricardokcavati@gmail.com>
2022-03-25 13:57:53 -0300

    Adição da biblioteca de busca na Epic, tratamento para busca do preço em reais, criação de método para busca na Epic, ajustes .gitignore.

--- GamePriceFinder/PriceSearcher.cs
+++ GamePriceFinder/PriceSearcher.cs
@@ -1,6 +1,8 @@
 ﻿using GamePriceFinder.Models;
 using GamePriceFinder.Responses;
+using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Web;
 
 namespace GamePriceFinder
 {
@@ -8,21 +10,55 @@ namespace GamePriceFinder
     {
         private const string steamUri = "http://store.steampowered.com/api/";
         private const int forHonorId = 304390;
-        public async Task GetPrices(string gameName)
+
+        private readonly HttpClient _client;
+
+        public PriceSearcher()
         {
-            await GetSteamPrice();
+            _client = new HttpClient();
+        }
+
+        private const string epicUri = "https://graphql.epicgames.com/graphql";
+        public async Task<List<String>> GetPrices(string gameName)
+        {
+            var forHonorSteam = await GetSteamPrice();
+            var forHonorEpic = await GetEpicPrice(gameName);
+
+            return new List<string> { forHonorSteam.Name, forHonorEpic.Name };
+        }
+
+        private async Task<Game> GetEpicPrice(string gameName)
+        {
+            var encoded = HttpUtility.UrlEncode(gameName).Replace(":", "%3A");
+            var request = new EpicGamesStoreNET.Models.Request(encoded);
+            var payload = JsonConvert.SerializeObject(request);
+
+            payload = payload.ToString().Replace("US", "BR");
+            payload = payload.ToString().Replace("en-US", "pt-BR");
+
+            var method = new HttpMethod("POST");
+            HttpContent body = new StringContent(payload);
+            body.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            var resp = await _client.PostAsync(epicUri, body);
+            var respString = await resp.Content.ReadAsStringAsync();
+
+            var converted = JsonConvert.DeserializeObject<EpicGamesStoreNET.Models.Response>(respString);
+
+            //TODO: Tratamento do preço da epic.
+
+            return new Game { Name = converted.Data.Catalog.SearchStore.Elements[0].Title };
         }
 
         private async Task<Game> GetSteamPrice()
         {
             var parameters = $"appdetails?appids={forHonorId}&cc=br&l=br";
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(steamUri);
+            _client.BaseAddress = new Uri(steamUri);
 
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            var response = client.GetAsync(parameters).Result;
+            var response = _client.GetAsync(parameters).Result;
 
             var jsonString = await response.Content.ReadAsStringAsync();
 
@@ -30,13 +66,14 @@ namespace GamePriceFinder
 
             var game = new Game();
 
+            //TODO: Tratamento do preço da steam.
             for (int responseObject = 0; responseObject < steamResponse.Count; responseObject++)
             {
                 game.Name = steamResponse["304390"].data.name;
                 var prices = steamResponse["304390"].data.price_overview;
             }
 
-            return new Game();
+            return game;
         }
 
     }
--- GamePriceFinder/Program.cs
+++ GamePriceFinder/Program.cs
@@ -3,13 +3,15 @@ using GamePriceFinder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//app.MapGet("/", () => "Hello World!");
-const int tropicoId = 57690;
-const int rSixId = 1922250;
 app.MapGet("/", async() =>
 {
     var priceSearcher = new PriceSearcher();
-    await priceSearcher.GetPrices("");
+    var games = await priceSearcher.GetPrices("for honor");
+
+    foreach (var game in games)
+    {
+        global::System.Console.WriteLine(game);
+    }
 });
 
 app.Run();
